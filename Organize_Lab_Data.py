3c28868e0a541df4e8c7943afd017063abc299d0
Ethan Gramowski <gramo012@umn.edu>
2024-06-03 13:52:18 -0500

    fixed spelling error

--- Organize_Lab_Data.py
+++ Organize_Lab_Data.py
@@ -14,17 +14,18 @@ def files(path):
 file_names = list(files(folder))
 files2remove = []
 
+#Creates new folders
 for n in range(199, 513):
     os.makedirs(os.path.join(folder, 'M_' + str(n)), exist_ok=True)
 
+#Copies files from main path, sorts into corresponding folder
 for n in range(199, 513):
     for file in file_names:
         if "M_" + str(n) in file:
             shutil.copy(os.path.join(folder, file), os.path.join(folder, 'M_' + str(n)))
             files2remove.append(os.path.join(folder, file))
 
+#Removes files from main path
 files2remove = list(set(files2remove))
-
-
 for file in files2remove:
     os.remove(file)
